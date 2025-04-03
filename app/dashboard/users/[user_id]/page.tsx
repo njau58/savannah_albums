af05bc46cfeda43fc00cee03d965f4f8dbd8b4b5
njauprince@gmail.com <njauprince@gmail.com>
2025-04-03 15:02:28 +0300

    fix param type user-page

--- app/dashboard/users/[user_id]/page.tsx
+++ app/dashboard/users/[user_id]/page.tsx
@@ -6,7 +6,7 @@ interface UserPageProps {
 }
 
 export default async function UserPage({ params }: UserPageProps) {
-	const { user_id } = await params
+	const { user_id } = params
 	return (
 		<div className="w-full max-w-7xl px-4 lg:px-0 mx-auto  pt-24">
 			<UserProfile user_id={user_id} />
